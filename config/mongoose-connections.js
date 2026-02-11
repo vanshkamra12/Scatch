085ded458190ea3a8ba7cd5e701eb0a4d5b78b45
Vansh Kamra <vanshkamra27@gmail.com>
2026-02-11 17:42:49 +0530

    Created owner creation route

--- config/mongoose-connections.js
+++ config/mongoose-connections.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose")
+const dbgr = require("debug")("development:mongoose");
+const config = require("config")
 
-mongoose.connect("mongodb://127.0.0.1:27017/scatch")
+
+mongoose
+.connect(`${config.get("MONGODB_URI")}/scatch`)
 .then(function(){
-    console.log("Connected to MongoDB..")
+    dbgr("Connected to MongoDB..")
 })
 .catch(function(err){
     console.log(err)
--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,8 +1,36 @@
 const express = require("express")
 const router = express.Router();
+const ownerModel = require("../models/owner-model")
+
+
+
+
+if(process.env.NODE_ENV === "development"){
+    router.post("/create" , async (req, res) =>{
+        let owners = await ownerModel.find();
+        if(owners.length > 0){
+            return res.status(500).send("Owner already exists")
+        }
+        let { fullname , email , password } = req.body;
+        let createdOwner = await ownerModel.create({
+            fullname,
+            email,
+            password,
+        })
+        res.status(201).send(createdOwner)
+
+
+
+})
+}
 
 router.get("/" , (req, res) =>{
     res.send("Owners Router")
 })
+
+
+
+
+
 
 module.exports = router;
